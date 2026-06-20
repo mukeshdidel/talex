1ec56997e22e4be868aebe15fa1cb8d9d15b1a26
mukeshdidel <mukeshdidel11111@gmail.com>
2026-06-20 11:54:18 +0530

    init

---
+++ talex-be/src/main/java/com/talex/talex/TalexApplication.java
@@ -0,0 +1,13 @@
+package com.talex.talex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalexApplication.class, args);
+	}
+
+}
---
+++ talex-be/src/test/java/com/talex/talex/TalexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talex.talex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
